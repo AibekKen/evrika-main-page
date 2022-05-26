df29ff34fc09a5505faf4daeb56e516ced5a281d
Aibek <ai.bek@mail.ru>
2022-05-26 21:04:13 +0600

    feat: add components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template >
   <div class="app">
     <header-body />
+    <div class="container">
+      <div class="block">
+        <sidebar-category />
+        <big-banner />
+      </div>
+      <news-block />
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderBody from "./components/HeaderBody.vue";
+import SidebarCategory from "./components/SidebarCategory.vue";
+import BigBanner from "./components/BigBanner.vue";
+import NewsBlock from "./components/NewsBlock.vue";
 
 export default {
   name: "App",
   components: {
     HeaderBody,
+    SidebarCategory,
+    BigBanner,
+    NewsBlock,
   },
 };
 </script>
@@ -20,7 +33,8 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
+  background: #f7f7f7;
 
   $fontfamily: "Manrope";
   $minwidth: 320px;
@@ -36,5 +50,18 @@ export default {
   max-width: 1440px;
   padding: 0 48px;
   margin: 0 auto;
+}
+
+.block {
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  margin: 0 0 32px 0;
+}
+
+.title {
+  font-weight: 800;
+  font-size: 28.5px;
+  color: #000000;
 }
 </style>
