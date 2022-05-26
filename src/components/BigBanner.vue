cd656f7ab0c7b41b0d496786516f387571f4b0b5
Aibek <ai.bek@mail.ru>
2022-05-26 21:00:30 +0600

    feat: add big banner component

---
+++ src/components/BigBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="banner">
+    <img src="@/assets/images/banner.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.banner {
+  flex: 0 1 calc((1037 / 1344) * 100%);
+}
+.banner img {
+  width: 100%;
+}
+</style>
