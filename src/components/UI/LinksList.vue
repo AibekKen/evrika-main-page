eb9577367c97e8c86c7d92290dcdcda047818fa0
Aibek <ai.bek@mail.ru>
2022-05-27 13:32:33 +0600

    feat: add link list components

---
+++ src/components/UI/LinksList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer-bottom__column links-block">
+    <div class="subtitle links-block__title">
+      {{ listBlock.title }}
+    </div>
+    <ul class="links-block__list">
+      <li v-for="link in listBlock.links" :key="link">
+        <a href="#" class="text links-block__link">{{ link }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    listBlock: {
+      type: Object,
+    },
+  },
+};
+</script>
+<style lang="scss">
+.footer-bottom {
+  &__column {
+  }
+}
+.links-block {
+  &__title {
+  }
+
+  &__list {
+  }
+
+  &__link:hover {
+    color: #fb791b;
+  }
+}
+.subtitle {
+}
+.text {
+}
+</style>
